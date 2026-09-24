95d1f2928ca01d2e701540f84159dbc69b51e40d
Ehsanullah Jan <ehsanullahjan@gmail.com>
2026-09-24 01:28:54 -0400

    Rename sysmon.sh to montools.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -euxo pipefail
 /ctx/build_files/install/libvirt.sh
 /ctx/build_files/install/vpn.sh
 
-/ctx/build_files/install/sysmon.sh
+/ctx/build_files/install/montools.sh
 
 /ctx/build_files/install/carapace.sh
 /ctx/build_files/install/dysk.sh
