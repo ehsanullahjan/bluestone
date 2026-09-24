9aaaaf2f774d0a9184463fd8ac71a29cc13697e9
Ehsanullah Jan <ehsanullahjan@gmail.com>
2026-09-24 01:28:12 -0400

    Rename tailscale.sh to vpn.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -euxo pipefail
 /ctx/build_files/install/docker.sh
 /ctx/build_files/install/podman.sh
 /ctx/build_files/install/libvirt.sh
-/ctx/build_files/install/tailscale.sh
+/ctx/build_files/install/vpn.sh
 
 /ctx/build_files/install/sysmon.sh
 
